56dea03bad39fbbe24e4fdc5f4daa8ca30cfd824
andrew titovskiy <titovskiy_andrew@mail.ru>
2023-06-08 00:22:05 +0600

    Добавил доп задачи

---
+++ task60/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0) 27(0,0,1) 90(0,1,1)
+// 34(1,0,0) 41(1,1,0) 26(1,0,1) 55(1,1,1)
+
+int[,,] Create3dMatrixRndInt(int row, int col, int dep)
+{
+    int[,,] matrix = new int[row, col, dep];
+    int number = 10;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = number++;
+            }
+        }
+    }
+    return matrix;
+}
+
+void Print3dMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}[{i}, {j}, {k}]  ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество сторк матрицы: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите глубину матрицы: ");
+int depth = Convert.ToInt32(Console.ReadLine());
+
+int[,,] array3d = Create3dMatrixRndInt(rows, columns, depth);
+Print3dMatrix(array3d);
---
+++ task62hw/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+void FillSpiralArray(int[,] matrix)
+    {
+        int number = 1;
+        int topRow = 0;
+        int bottomRow = matrix.GetLength(0) - 1;
+        int leftColumn = 0;
+        int rightColumn = matrix.GetLength(1) - 1;
+
+        while (number <= matrix.GetLength(0) * matrix.GetLength(1))
+        {
+            for (int i = leftColumn; i <= rightColumn; i++)
+            {
+                matrix[topRow, i] = number;
+                number++;
+            }
+            topRow++;
+
+            for (int i = topRow; i <= bottomRow; i++)
+            {
+                matrix[i, rightColumn] = number;
+                number++;
+            }
+            rightColumn--;
+
+            for (int i = rightColumn; i >= leftColumn; i--)
+            {
+                matrix[bottomRow, i] = number;
+                number++;
+            }
+            bottomRow--;
+
+            for (int i = bottomRow; i >= topRow; i--)
+            {
+                matrix[i, leftColumn] = number;
+                number++;
+            }
+            leftColumn++;
+        }
+    }
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество сторк матрицы: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+    int[,] spiralArray = new int[rows, columns];
+        FillSpiralArray(spiralArray);
+        PrintMatrix(spiralArray);
